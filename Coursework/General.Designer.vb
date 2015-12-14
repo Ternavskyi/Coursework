b5b72b0fdadbae4ccbda8082ad82666fd47598ec
Roman Ternavskyi <ternavskyi.roman@gmail.com>
2015-12-15 01:17:17 +0200

    Start sorting methods

--- Coursework/General.Designer.vb
+++ Coursework/General.Designer.vb
@@ -22,8 +22,29 @@ Partial Class General
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(259, 236)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'General
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "General"
         Me.Text = "General"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
--- Coursework/General.vb
+++ Coursework/General.vb
@@ -1,3 +1,5 @@
 ﻿Public Class General
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Sort()
+    End Sub
 End Class
